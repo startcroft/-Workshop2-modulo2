7f93234945a712ff001034d6b57d013898f1a49b
startcroft <santiagogomezpavas42@gmail.com>
2023-04-28 23:31:07 -0500

    deploy 2

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,25 @@
 import "/src/app/styles/styles.css";
+const feature_list = document.querySelector(".feature_list");
+const list_company = document.querySelector(".list_company");
+const list_feature = document.querySelector(".list_feature");
+const features = document.querySelector(".features");
+const company = document.querySelector(".company");
+const nav_button = document.querySelector(".nav_button");
+const components = document.querySelector(".components");
+const close_button = document.querySelector(".close_button");
+
+features.addEventListener(`click`, () =>{
+    list_feature.classList.toggle("dropDown");
+})
+
+company.addEventListener(`click`, () =>{
+    list_company.classList.toggle("dropDown");
+})
+
+nav_button.addEventListener(`click`, () =>{
+  components.classList.toggle("dropDown");
+})
+
+close_button.addEventListener(`click`, () => {
+    components.classList.remove("dropDown");
+})
